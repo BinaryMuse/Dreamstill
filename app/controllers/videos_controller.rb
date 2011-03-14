cd5c3c300cd22073f2f23cbc13067ab7371907f1
Justin Meltzer <meltzerj@wharton.upenn.edu>
2011-03-14 14:41:33 -0700

    modal and partials

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,8 @@
 class VideosController < ApplicationController
   
-#  def index
-#    @videos = Video.all
-#  end
+  def index
+    @videos = Video.all
+  end
 
   def show
     @video = Video.find(params[:id])
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,11 +7,49 @@ $(document).ready(function() {
 		$("#video_div").prepend($("<img>", { src: oembed.thumbnail_url, width:200 }));
 	});
 	
-	$("#video_div img").click(function() {
+	$("#video_div img").live('click', function() {
+	   $(this).toggle();	
 	   $("div.embed").toggle();
 	});
 	
 	
+	$('a[name=modal]').click(function(e) {
+	        //Cancel the link behavior
+	        e.preventDefault();
+	        //Get the A tag
+	        var id = $(this).attr('href');
+
+	        //transition effect     
+	        $('#mask').fadeIn(50);    
+
+	        //Get the window height and width
+	        var winH = $(window).height();
+	        var winW = $(window).width();
+	
+			$('#mask').css('top',  winH/2-$('#mask').height()/2.35);
+	        $('#mask').css('left', winW/2-$('#mask').width()/2.2);
+
+	        //Set the popup window to center
+	        $(id).css('top',  winH/2-$(id).height()/2);
+	        $(id).css('left', winW/2-$(id).width()/2);
+
+	        //transition effect
+	        $(id).fadeIn(50); 
+
+	    });
+
+	    //if close button is clicked
+	    $('.window .close').click(function (e) {
+	        //Cancel the link behavior
+	        e.preventDefault();
+	        $('#mask, .window').hide();
+	    });     
+
+	    //if mask is clicked
+	    $('#mask').click(function () {
+	        $(this).hide();
+	        $('.window').hide();
+	    });	
 });
 
 
